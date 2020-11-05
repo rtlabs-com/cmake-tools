31379ea94acb355810df85b1191a9c7971dd2337
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2020-11-05 09:45:41 +0100

    Update OSAL version

--- AddOsal.cmake
+++ AddOsal.cmake
@@ -22,7 +22,7 @@ if (NOT Osal_FOUND)
   FetchContent_Declare(
     Osal
     GIT_REPOSITORY      https://github.com/rtlabs-com/osal.git
-    GIT_TAG             34fe1cc
+    GIT_TAG             e2695b1
     )
   FetchContent_MakeAvailable(Osal)
 endif()
