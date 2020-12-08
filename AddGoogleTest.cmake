7d7cab70f67f240f29f9f65425db445cf9b1df26
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2020-12-08 11:34:22 +0100

    Update gtest
    Update gtest to latest from upstream. Avoid installing gtest when
    building install target. Fix build issues on rt-kernel.

--- AddGoogleTest.cmake
+++ AddGoogleTest.cmake
@@ -6,17 +6,21 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 include(FetchContent)
 FetchContent_Declare(
   googletest
-#  GIT_REPOSITORY      https://github.com/google/googletest.git
-#  GIT_TAG             release-1.10.0
   GIT_REPOSITORY      https://github.com/rtlabs-com/googletest.git
-  GIT_TAG             cc602bd729c3acd610c3de5d65d1f1d598c41522
+  GIT_TAG             4847f25e50fc6d488bf202292c9f5745bd4fe7bb
   )
 FetchContent_GetProperties(googletest)
 if(NOT googletest_POPULATED)
   FetchContent_Populate(googletest)
   set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE BOOL "")
+  set(CMAKE_WARN_DEPRECATED 0)
+  set(INSTALL_GTEST OFF CACHE BOOL "")
   add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+  if (CMAKE_SYSTEM_NAME STREQUAL rt-kernel)
+    target_compile_options(gtest PRIVATE -D_POSIX_C_SOURCE=200809L -Wno-psabi)
+  endif()
   unset(CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
+  unset(CMAKE_WARN_DEPRECATED)
 endif()
 
 add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND}
