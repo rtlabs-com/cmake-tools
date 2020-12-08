c1fd4ac9179b4e78dcb5c725395c6cbe7ac09c4c
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2020-12-08 11:36:35 +0100

    Build osal as static library, do not install
    Always build OSAL as static library if it was downloaded locally.
    
    Replace FetchContent_MakeAvailable to be able to set EXCLUDE_FROM_ALL
    which prevents osal from being installed with the project.

--- AddOsal.cmake
+++ AddOsal.cmake
@@ -15,14 +15,31 @@
 
 cmake_minimum_required(VERSION 3.14)
 
+# Attempt to find externally built OSAL
 find_package(Osal QUIET)
 
 if (NOT Osal_FOUND)
+  # Download and build OSAL locally as a static library
   include(FetchContent)
   FetchContent_Declare(
-    Osal
+    osal
     GIT_REPOSITORY      https://github.com/rtlabs-com/osal.git
     GIT_TAG             44e5a39
     )
-  FetchContent_MakeAvailable(Osal)
+  FetchContent_GetProperties(osal)
+  if(NOT osal_POPULATED)
+    FetchContent_Populate(osal)
+    set(BUILD_SHARED_LIBS_OLD ${BUILD_SHARED_LIBS})
+    set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "" FORCE)
+    add_subdirectory(${osal_SOURCE_DIR} ${osal_BINARY_DIR} EXCLUDE_FROM_ALL)
+    set(BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS_OLD} CACHE BOOL "" FORCE)
+    get_property(SUPPORTS_SHARED GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS)
+    if (SUPPORTS_SHARED)
+      set_property(TARGET osal PROPERTY POSITION_INDEPENDENT_CODE ON)
+    endif()
+  endif()
+
+  # Hide Osal_DIR to avoid confusion, as it is not used in this
+  # configuration
+  mark_as_advanced(Osal_DIR)
 endif()
