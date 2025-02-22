f9c3f64b59cfbd1530f245f3c2ba06b31a7bc9a8
Joakim Plate <joakim.plate@rt-labs.com>
2025-02-22 10:06:49 +0100

    fix: ensure generate_bin handle changed filename
    The targetname is not always the final file name,
    it can be in a different directory and it can have
    different suffixes applied.

--- Platform/STM32Cube.cmake
+++ Platform/STM32Cube.cmake
@@ -75,6 +75,6 @@ set(CMAKE_CXX_STANDARD_LIBRARIES "${LIBS} -lstdc++")
 macro(generate_bin TARGET)
   add_custom_command(TARGET ${TARGET} POST_BUILD
     COMMAND ${OBJCOPY}
-    ARGS -O binary ${TARGET}.elf ${TARGET}.bin
+    ARGS -O binary $<TARGET_FILE:${TARGET}> $<TARGET_FILE_DIR:${TARGET}>/$<TARGET_FILE_BASE_NAME:${TARGET}>.bin
     )
 endmacro()
